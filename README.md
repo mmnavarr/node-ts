5807e090271b539503f7d07c38cd569a99fac803
Malcolm Navarro <mmnavarr@live.com>
2021-06-02 11:17:57 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# TypeScript WebSocket Client Service
+
+## Running the WS Service
+Before running the websocket service make sure you have the latest code.
+
+### Install Packages
+```
+npm install
+```
+
+### Run the service
+```
+npm run start
+```
---
+++ index.ts
@@ -0,0 +1,28 @@
+import http from "http";
+
+const host = "localhost";
+const port = 8000;
+
+// const server = http.createServer();
+// server.listen(port, host, () => console.log(`Server started on port: ${port}`));
+
+class Server {
+  constructor() {
+    this.initialize();
+  }
+
+  private async initialize() {
+    this.initializeHttpServer();
+  }
+
+  private initializeHttpServer() {
+    // Create http server
+    const server = http.createServer();
+
+    server.listen(port, host, () =>
+      console.log(`Server started on port: ${port}`)
+    );
+  }
+}
+
+const server = new Server();
